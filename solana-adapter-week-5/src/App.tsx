d6aa23c4067e16f7e86523195ffbdc1d5b1420bd
C0dewithLokesh <lokeshkaushik2243@gmail.com>
2024-09-15 18:10:30 +0530

    show wallet balance functionality done

--- solana-adapter-week-5/src/App.tsx
+++ solana-adapter-week-5/src/App.tsx
@@ -1,10 +1,28 @@
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import {
   WalletDisconnectButton,
   WalletMultiButton,
 } from "@solana/wallet-adapter-react-ui";
+import { useEffect, useState } from "react";
 import Airdrop from "./components/Airdrop";
 
 function App() {
+  const { connection } = useConnection();
+  const wallet = useWallet();
+  const [walletBalance, setWalletBalance] = useState<string | number>("");
+
+  const getWalletBalance = async () => {
+    if (wallet?.publicKey) {
+      console.log(123);
+      const balance = await connection.getBalance(wallet.publicKey);
+      setWalletBalance(balance);
+    }
+  };
+  console.log(23);
+  useEffect(() => {
+    getWalletBalance();
+  }, []);
+
   return (
     <div className="flex flex-col items-center gap-6">
       <div className="flex items-center w-full justify-between p-5">
@@ -17,6 +35,10 @@ function App() {
         />
         <WalletDisconnectButton />
       </div>
+      <p className="text-xl">
+        Wallet Balance:{" "}
+        <span className="text-[#512da8] font-medium">{walletBalance}</span>
+      </p>
       <Airdrop />
     </div>
   );
