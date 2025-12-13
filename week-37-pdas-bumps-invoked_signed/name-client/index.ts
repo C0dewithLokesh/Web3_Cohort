5db9a672a89149249054d4a33b922339496b9a29
C0dewithLokesh <lokeshkaushik2243@gmail.com>
2025-12-14 05:09:21 +0530

    create data account functionality added

--- week-37-pdas-bumps-invoked_signed/name-client/index.ts
+++ week-37-pdas-bumps-invoked_signed/name-client/index.ts
@@ -1,21 +1,44 @@
-import { Connection, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import {
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  sendAndConfirmTransaction,
+  SystemProgram,
+  Transaction,
+} from "@solana/web3.js";
 
 const connection = new Connection("http://127.0.0.1:8899");
+const space = 8;
+const lamports = await connection.getMinimumBalanceForRentExemption(space);
 
 async function main() {
   const kp = new Keypair();
+  const dataAccount = new Keypair();
+
   const res = await connection.requestAirdrop(
     kp.publicKey,
     3 * LAMPORTS_PER_SOL
   );
+
   const latestBlockhash = await connection.getLatestBlockhash();
   await connection.confirmTransaction({
     signature: res,
     blockhash: latestBlockhash.blockhash,
     lastValidBlockHeight: latestBlockhash.lastValidBlockHeight,
   });
-  const balance = await connection.getBalance(kp.publicKey);
-  console.log("balance", balance);
+
+  const instruction = SystemProgram.createAccount({
+    fromPubkey: kp.publicKey,
+    lamports,
+    programId: SystemProgram.programId,
+    newAccountPubkey: dataAccount.publicKey,
+    space,
+  });
+
+  const tx = new Transaction().add(instruction);
+
+  await sendAndConfirmTransaction(connection, tx, [kp, dataAccount]);
+  console.log(dataAccount.publicKey.toString());
 }
 
 main();
